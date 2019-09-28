a826375ed97f292590202663f3d77c636aefb081
zoey <740357960@qq.com>
2019-09-28 23:31:28 +1000

    Delete the bottom navigation, add add button and question button on the bar.

---
+++ app/src/main/java/com/comp6442/group/timetable/EnrolActivity.java
@@ -0,0 +1,14 @@
+package com.comp6442.group.timetable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnrolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enrol);
+        setTitle("Enrol");
+    }
+}
--- app/src/main/java/com/comp6442/group/timetable/MainActivity.java
+++ app/src/main/java/com/comp6442/group/timetable/MainActivity.java
@@ -1,9 +1,11 @@
 package com.comp6442.group.timetable;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -11,34 +13,32 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = null;
+        int id = item.getItemId();
+        if (id == R.id.btn_add) {
+            // do something here
+            intent = new Intent(MainActivity.this, EnrolActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.btn_question) {
+            // do something else
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
