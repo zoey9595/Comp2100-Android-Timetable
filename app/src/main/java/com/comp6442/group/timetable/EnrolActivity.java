e9fb694c19a40648903a91117b5935c5760e0940
zoey <740357960@qq.com>
2019-10-01 09:03:43 +1000

    Add essential comments

--- app/src/main/java/com/comp6442/group/timetable/EnrolActivity.java
+++ app/src/main/java/com/comp6442/group/timetable/EnrolActivity.java
@@ -33,6 +33,7 @@ public class EnrolActivity extends AppCompatActivity {
         setContentView(R.layout.activity_enrol);
         setTitle("Enrol");
 
+        // Set up variables
         mTvCourseID = findViewById(R.id.tv_courseID);
         mTvLectureDetails = findViewById(R.id.tv_lectureDetails);
         mBtnEnrol = findViewById(R.id.btn_enrol);
@@ -43,27 +44,30 @@ public class EnrolActivity extends AppCompatActivity {
         final ScrollView scrollView = findViewById(R.id.scrollable);
         final LinearLayout linearLayout = findViewById(R.id.ll_scroll);
 
+        // Get a list containing all ANU courses
         final Course course = Course.getCourseInstance(this);
         courseList = (ArrayList) course.getCourseList();
 
+        // Make lecture details can be scrolled
         mTvLectureDetails.setMovementMethod(new ScrollingMovementMethod());
 
+        // Set up an OnClickListener on the searchview
         mBtnSearchCourse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final Dialog dialog = new Dialog(view.getContext());
                 LayoutInflater inflater = LayoutInflater.from(view.getContext());
-                View view1 = inflater.inflate(R.layout.searchable_list_dialog, null);
-                final ListView listView = view1.findViewById(R.id.lv_course);
-                final SearchView searchView = view1.findViewById(R.id.sv_course);
-
+                View searchableView = inflater.inflate(R.layout.searchable_list_dialog, null);
+                final ListView mLvCourse = searchableView.findViewById(R.id.lv_course);
+                final SearchView mSvCourse = searchableView.findViewById(R.id.sv_course);
+                // Set adapter
                 final ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, courseList);
-                listView.setAdapter(stringArrayAdapter);
-
-                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                mLvCourse.setAdapter(stringArrayAdapter);
+                // Make course searchview listen to text change
+                mSvCourse.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
                     @Override
                     public boolean onQueryTextSubmit(String newText) {
-                        searchView.clearFocus();
+                        mSvCourse.clearFocus();
                         return true;
                     }
 
@@ -77,35 +81,36 @@ public class EnrolActivity extends AppCompatActivity {
                         return true;
                     }
                 });
-
-                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                // make listview show the selected course lecture time
+                mLvCourse.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                         courseID = stringArrayAdapter.getItem(i);
                         mTvCourseID.setText(courseID);
                         lectureList = (ArrayList) course.getLessons(courseID);
                         StringBuilder builder = new StringBuilder();
-                        for (Object s: lectureList){
-                            builder.append(s+"\n");
+                        for (Object s : lectureList) {
+                            builder.append(s + "\n");
                         }
                         mTvLectureDetails.setText(builder.toString());
                         linearLayout.removeAllViews();
                         dialog.dismiss();
                     }
                 });
-                dialog.setContentView(view1);
+                dialog.setContentView(searchableView);
                 dialog.show();
             }
         });
-
+        // dynamically increase the amount of tutorial checkboxes
         mBtnSearchTutorial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 tutorialList = (ArrayList) course.getLessons(courseID);
-                for (int i=0; i<tutorialList.size();i++){
+                for (int i = 0; i < tutorialList.size(); i++) {
                     CheckBox checkBox = new CheckBox(view.getContext());
                     checkBox.setId(i);
                     checkBox.setText(tutorialList.get(i).toString());
+                    checkBox.setTextColor(-1979711488);
                     linearLayout.addView(checkBox);
                 }
             }
