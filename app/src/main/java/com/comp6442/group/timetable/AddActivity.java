4e69c800e9a7c952f434813554fa24d9f0ec235a
Xiaochan Zhang <u6855326@anu.edu.au>
2019-10-04 14:38:08 +1000

    not finish course detail part

--- app/src/main/java/com/comp6442/group/timetable/AddActivity.java
+++ app/src/main/java/com/comp6442/group/timetable/AddActivity.java
@@ -13,7 +13,14 @@ import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    //declare all the variables of widgets
     private Spinner spinner_semester;
+    private Spinner spinner_day;
+    private Spinner spinner_class1;
+    private Spinner spinner_class2;
+    private Spinner spinner_class3;
+    private Spinner spinner_statime;
+    private Spinner spinner_endtime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,28 +28,80 @@ public class AddActivity extends AppCompatActivity implements AdapterView.OnItem
         setContentView(R.layout.activity_add);
         setTitle("Add Course");
 
+        //initControl();
+
         //spinner that choose semester or spring/summer/autumn/winter section of the courses
         spinner_semester = findViewById(R.id.spinner_semester);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.semester, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        spinner_semester.setAdapter(adapter);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.semester, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_semester.setAdapter(adapter1);
         spinner_semester.setOnItemSelectedListener(this);
+
+        //spinner that choose day
+        spinner_day = findViewById(R.id.spinner_day);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.day, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_day.setAdapter(adapter2);
+        spinner_day.setOnItemSelectedListener(this);
+
+        //*******************一行的布局*****************
+        spinner_class1 = findViewById(R.id.spinner_1);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this, R.array.class1, android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_class1.setAdapter(adapter4);
+        spinner_class1.setOnItemSelectedListener(this);
+
+        spinner_class2 = findViewById(R.id.spinner_2);
+        ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this, R.array.class2, android.R.layout.simple_spinner_item);
+        adapter5.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_class2.setAdapter(adapter5);
+        spinner_class2.setOnItemSelectedListener(this);
+
+        spinner_class3 = findViewById(R.id.spinner_3);
+        ArrayAdapter<CharSequence> adapter6 = ArrayAdapter.createFromResource(this, R.array.class3, android.R.layout.simple_spinner_item);
+        adapter6.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_class3.setAdapter(adapter6);
+        spinner_class3.setOnItemSelectedListener(this);
+
+        spinner_day = findViewById(R.id.spinner_4);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.day, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_day.setAdapter(adapter3);
+        spinner_day.setOnItemSelectedListener(this);
+
+        spinner_statime = findViewById(R.id.spinner_5);
+        ArrayAdapter<CharSequence> adapter7 = ArrayAdapter.createFromResource(this, R.array.start, android.R.layout.simple_spinner_item);
+        adapter7.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_statime.setAdapter(adapter7);
+        spinner_statime.setOnItemSelectedListener(this);
+
+        spinner_endtime = findViewById(R.id.spinner_6);
+        ArrayAdapter<CharSequence> adapter8 = ArrayAdapter.createFromResource(this, R.array.end, android.R.layout.simple_spinner_item);
+        adapter8.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_endtime.setAdapter(adapter8);
+        spinner_endtime.setOnItemSelectedListener(this);
+
+    }
+
+    /* This is a method that init all the widget
+    * */
+    private void initControl(){
+        //find all the widgets;
+
 
     }
 
     public void saveButton(View view) {
     }
 
-    public void addDetail(View view) {
+    public void cancelButton(View view) {
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        ((TextView)parent.getChildAt(0)).setTextSize(20);
+        ((TextView)parent.getChildAt(0)).setTextSize(15);
         ((TextView)parent.getChildAt(0)).setTextColor(getResources().getColor(R.color.colorPrimaryDark));
     }
     @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
+    public void onNothingSelected(AdapterView<?> parent) {}
 }
