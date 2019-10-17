532695a8eed22d3490a2613b7d5b908b8372946f
Yongchao Lyu <u6874539@anu.edu.au>
2019-10-17 23:20:19 +1100

    Add comments for the class LessonBlock.

--- app/src/main/java/com/comp6442/group/timetable/LessonBlock.java
+++ app/src/main/java/com/comp6442/group/timetable/LessonBlock.java
@@ -1,3 +1,12 @@
+/**
+ * The LessonBlock class is a subclass of AppCompatTextView,
+ * which is used to render lesson block on the timetable
+ *
+ * @author  Yongchao Lyu (u6874539)
+ * @version 1.0
+ * @since   2019-09-18
+ */
+
 package com.comp6442.group.timetable;
 
 import android.content.Context;
@@ -9,7 +18,12 @@ import android.support.v7.widget.AppCompatTextView;
 import android.view.Gravity;
 
 public class LessonBlock extends AppCompatTextView {
-
+    /**
+     * Public constructor of this class for initialising.
+     *
+     * @param context Context: the context of this application
+     *
+     */
     public LessonBlock(Context context) {
         super(context);
 
@@ -20,6 +34,12 @@ public class LessonBlock extends AppCompatTextView {
         setTextColor(ContextCompat.getColor(context, R.color.colorDarken));
     }
 
+    /**
+     * Sets the background color for this view.
+     *
+     * @param color int: the color of the background
+     *
+     */
     @Override
     public void setBackgroundColor(int color) {
         StateListDrawable background = new StateListDrawable();
@@ -32,6 +52,10 @@ public class LessonBlock extends AppCompatTextView {
         setBackgroundDrawable(background);
     }
 
+    /**
+     * Resets the background color for this view
+     *
+     */
     public void resetLessonBlock() {
         setText(null);
         setTag(null);
